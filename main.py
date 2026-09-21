7ea6e24e26c5ad1ebedf19e1a31a4cf3f96aea79
wagneralexander <alexander.wagner@liv.systems>
2026-09-21 09:39:41 +0200

    Implement Store class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from products import Product
+from store import Store
 
 bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
 mac = Product("MacBook Air M2", price=1450, quantity=100)
@@ -12,3 +13,14 @@ mac.show()
 
 bose.set_quantity(1000)
 bose.show()
+
+product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                Product("Google Pixel 7", price=500, quantity=250),
+               ]
+
+best_buy = Store(product_list)
+products = best_buy.get_all_products()
+print(best_buy.get_total_quantity())
+print(best_buy.order([(products[0], 1), (products[1], 2)]))
+print(best_buy.get_total_quantity())
---
+++ store.py
@@ -0,0 +1,30 @@
+from products import Product
+
+class Store:
+    def __init__(self, products):
+        self.products = products
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+    def get_total_quantity(self):
+        total = 0
+        for product in self.products:
+            total += product.get_quantity()
+        return total
+
+    def get_all_products(self):
+        active_products = []
+        for product in self.products:
+            if product.is_active():
+                active_products.append(product)
+        return active_products
+
+    def order(self, shopping_list):
+        total_price = 0
+        for product, quantity in shopping_list:
+            total_price += product.buy(quantity)
+        return total_price
